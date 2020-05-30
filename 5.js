e750a004d0d748bb0bb1e24fdecf4db2d200456a
“YutaYuki” <yuta@example.com>
2020-05-30 19:45:21 +0900

    update

---
+++ 5.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+async function getUsers() {
+  const res = await fetch("http://jsonplaceholder.typicode.com/users")
+  const users = await res.json();
+  return users;
+}
+
+function addList(user){
+    const list = document.createElement("li")
+    list.innerText = user.name;
+    lists.appendChild(list) 
+  }
+
+
+async function listUsers(){
+//データのやりとり
+ const users = await getUsers(); 
+//DOM操作
+  users.forEach(addList);
+ }
+
+button.addEventListener("click", listUsers);
+
+window.addEventListener("load", listUsers);
